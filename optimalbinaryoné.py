18b35e4c5e9c87bf53866624589a6eb2c52ddeec
Juraj Lapčák <lapcakjuraj@gmail.com>
2022-03-26 23:10:04 +0100

    bugfix: change to working indexes

--- optimalbinaryoné.py
+++ optimalbinaryoné.py
@@ -26,19 +26,19 @@ class BinarySearchTree:
 
 
 def optimal_bst(p_table, q_table, n):
-    e_table = [[0 for _ in range(n)] for _ in range(n + 1)]
-    w_table = [[0 for _ in range(n)] for _ in range(n + 1)]
-    root_table = [[0 for _ in range(n)] for _ in range(n + 1)]
+    e_table = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
+    w_table = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
+    root_table = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
 
-    for i in range(1, n + 1):
+    for i in range(1, n + 2):
         e_table[i][i - 1] = q_table[i - 1]
         w_table[i][i - 1] = q_table[i - 1]
 
-    for l in range(1, n):
-        for i in range(1, n - l + 1):
+    for l in range(1, n + 1):
+        for i in range(1, n - l + 2):
             j = i + l - 1
             e_table[i][j] = float('inf')
-            w_table[i][j] = w_table[i][j - 1] + p_table[j] + q_table[j]
+            w_table[i][j] = w_table[i][j - 1] + p_table[j - 1] + q_table[j]
 
             for r in range(i, j + 1):
                 t = e_table[i][r - 1] + e_table[r + 1][j] + w_table[i][j]
@@ -134,7 +134,7 @@ if __name__ == "__main__":
 
     e_table, root_table = optimal_bst(p_table, q_table, len(reduced_data))
 
-    print(len(e_table[1]))
+    print((e_table[1][len(reduced_data)]))
 
     # tmp_sum = 0
     # for i in q_table:
