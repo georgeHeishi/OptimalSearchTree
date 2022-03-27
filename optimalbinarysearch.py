a1f9fa8f33da22d7aac99fa82312167aa024aeeb
Juraj Lapčák <lapcakjuraj@gmail.com>
2022-03-27 17:37:03 +0200

    feat: Implement Optimal Binary Search

--- optimalbinarysearch.py
+++ optimalbinarysearch.py
@@ -9,27 +9,35 @@ This script was implemented as a solution to assigment on FEI STU course I-ADS A
 
 __author__ = 'Juraj Lapcak'
 
-
 import sys
+from binarytree import Node
 
 
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+# class Node:
+#     def __init__(self, value: str):
+#         self.value = value
+#         self.left = None
+#         self.right = None
 
 
 def optimal_bst(p_table, q_table, n):
-    e_table = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
-    w_table = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
-    root_table = [[0 for _ in range(n + 1)] for _ in range(n + 2)]
+    '''
+    Parameters:
+        p_table -- list of probabilities p
+        q_table -- list of probabilities q
+        n -- number of words
+    Returns:
+        e_table -- table of costs
+        root_table -- table of keys
+    '''
+    # first row will remain filled with None, because of indexing when implementing
+    # algorithm from book Introduction to Algorithms, Third Edition by Thomas H. Cormen ...
+    # page 402
+    e_table = [[None for _ in range(n + 1)] for _ in range(n + 2)]
+    w_table = [[None for _ in range(n + 1)] for _ in range(n + 2)]
+    root_table = [[None for _ in range(n + 1)] for _ in range(n + 2)]
 
+    # + 2 because python range function indexes in range <i, n)
     for i in range(1, n + 2):
         e_table[i][i - 1] = q_table[i - 1]
         w_table[i][i - 1] = q_table[i - 1]
@@ -38,6 +46,8 @@ def optimal_bst(p_table, q_table, n):
         for i in range(1, n - l + 2):
             j = i + l - 1
             e_table[i][j] = float('inf')
+
+            # p_table[j - 1].. -1 because we chose to index p table from 0 not from 1
             w_table[i][j] = w_table[i][j - 1] + p_table[j - 1] + q_table[j]
 
             for r in range(i, j + 1):
@@ -72,7 +82,7 @@ def calculate_q(data, reduced_data, freq_sum):
         data -- list of tuples, where first member is frequency, second word
         reduced_data -- list of tuples, with frequency >= 50_000, where first member is frequency, second word
         freq_sum --summary of frequencies
-    Return:
+    Returns:
         q_table -- list of probabilities q
     '''
     q_table = list()
@@ -102,7 +112,7 @@ def prepare_data(data_path):
     '''
     Parameters:
         data_path -- string, path to data-file
-    Return:
+    Returns:
         sorted_data -- list of tuples, where first member is frequency, second word
         sorted_reduced_data -- list of tuples, with frequency >= 50_000, where first member is frequency, second word
         freq_sum -- summary of frequencies
@@ -126,6 +136,64 @@ def prepare_data(data_path):
     return sorted_data, sorted_reduced_data, freq_sum
 
 
+def build_tree(root_table, reduced_data):
+    '''
+    Parameters:
+        root_table -- table of keys
+        reduced_data -- list of tuples, with frequency >= 50_000, where first member is frequency, second word
+    Returns:
+        root_node -- root of Optimal Search Tree
+    '''
+    root_index = root_table[1][-1]
+    root_node = Node(reduced_data[root_index - 1][1])
+    stack = [(root_node, 1, len(root_table) - 2)]
+
+    while stack:
+        node, row, col = stack.pop()
+        root_index = root_table[row][col]
+
+        if root_index < col:
+            node.right = Node(
+                reduced_data[root_table[root_index + 1][col] - 1][1])
+            stack.append((node.right, root_index + 1, col))
+
+        if row < root_index:
+            node.left = Node(
+                reduced_data[root_table[row][root_index - 1] - 1][1])
+            stack.append((node.left, row, root_index - 1))
+
+    return root_node
+
+
+def pocet_porovnani(find_word: str, root_node: Node):
+    '''
+    Parameters:
+        find_word -- string to search for in tree
+        root_node -- root of Optimal Search Tree    
+    Returns:
+        comparisons_cnt -- number of comparisons needed to find a word
+    '''
+    comparisons_cnt = 0
+    node = root_node
+
+    print('\nPath:')
+
+    while node != None:
+        comparisons_cnt += 1
+        print(f'{node.value} -> ', end=""),
+        if find_word == node.value:
+            print('SUCCESSFUL')
+            return comparisons_cnt
+
+        if find_word < node.value:
+            node = node.left
+        elif find_word > node.value:
+            node = node.right
+
+    print('UNSUCCESSFUL')
+    return comparisons_cnt
+
+
 if __name__ == "__main__":
     data, reduced_data, freq_sum = prepare_data('dictionary.txt')
 
@@ -134,27 +202,29 @@ if __name__ == "__main__":
 
     e_table, root_table = optimal_bst(p_table, q_table, len(reduced_data))
 
-    print((e_table[1][len(reduced_data)]))
+    with open('e_table.txt', 'w') as f:
+        for i in e_table:
+            f.write(f'{i}')
 
-    # tmp_sum = 0
-    # for i in q_table:
-    #     tmp_sum += i
-    # for i in p_table:
-    #     tmp_sum += i
-    # print(tmp_sum)
+    with open('root_table.txt', 'w') as f:
+        for i in root_table:
+            f.write(f'{i}')
 
-    # with open('data.txt', 'w') as f:
-    #     for i in data:
-    #         f.write(f'{i}\n')
+    root = build_tree(root_table, reduced_data)
 
-    # with open('reduced_data.txt', 'w') as f:
-    #     for i in reduced_data:
-    #         f.write(f'{i}\n')
+    while True:
+        print('.......SEARCH.......')
+        print('To quit enter q!\n')
 
-    # with open('p_table.txt', 'w') as f:
-    #     for i in p_table:
-    #         f.write(f'{i}\n')
+        find_word = str(input("Word to search for: "))
+        if find_word == 'q!':
+            break
+        comparisons_cnt = pocet_porovnani(find_word, root)
+        print(f'\nNumber of comparisons: {comparisons_cnt}.\n')
 
-    # with open('q_table.txt', 'w') as f:
-    #     for i in q_table:
-    #         f.write(f'{i}\n')
+    original_stdout = sys.stdout
+    with open('tree1.txt', 'w') as f:
+        sys.stdout = f
+        print()
+        print(root)
+        sys.stdout = original_stdout
